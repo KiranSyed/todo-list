0b593aa556f07b67b25c373da3c8c76f21fec568
kiran syed <kz_syed@yahoo.com>
2020-03-10 20:42:50 +0000

    add work list

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 var items = [];
+var workItems = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(express.static("public"))
 app.get("/", (req,res)=>{
 	const todaysDate = new Date();
 	const options = {
@@ -15,13 +17,26 @@ app.get("/", (req,res)=>{
 
 	}
 	let day = todaysDate.toLocaleDateString("en-US", options)
-	res.render("list", {day: day, newItem: items});
+	res.render("list", {listTitle: day, newListItems: items});
 });
 app.post("/", (req, res)=>{
 	let item = req.body.newItem;
-	items.push(item);
-	res.redirect("/");
-
+	console.log(req.body.list);
+	if (req.body.list==="Work")
+	{
+		workItems.push(item);
+		res.redirect("/work");
+	}
+	else
+	{
+		items.push(item);
+		res.redirect("/");
+	
+	}
+	
+});
+app.get("/work", (req,res)=>{
+	res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 app.listen(port, ()=>{
 	console.log("server started ...");
